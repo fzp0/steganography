a3147f6b8cbf7e308809184d11b327698365cc93
fzp0 <44344990+fzp0@users.noreply.github.com>
2024-05-23 09:59:54 +0200

    changes.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,75 @@
 #include <iostream>
 #include <fmt/core.h>
 #include <filesystem>
+#include <chrono>
 #include "read_picture.h"
 #include <SFML/Graphics/Image.hpp>
 
 
-void display_file_info(std::filesystem::path const& file) {
+void display_image_info(sf::Image& image){
+    auto sizevec = image.getSize();
+    fmt::println("BMP Width: {}, BMP Height: {}", sizevec.x, sizevec.y);
+}
+
+
+void handle_file_extension(std::filesystem::path const& file) {
     fmt::println("Name: {}", file.filename().string());
     fmt::println("Size: {} bytes",file_size(file));
+    auto p = std::filesystem::last_write_time(file);
+    auto c = file
+    fmt::println("Last time modified: {:%Y-%m-%d %H:%M:%S}", p);
     std::string extension = file.extension().string();
+    auto image = sf::Image();
+    if(!image.loadFromFile(file.string())){
+        fmt::println("invalid file - loadFromFile() failed");
+        exit(-1);
+    }
+
+    //no switch case for string :c
     if(extension == ".bmp"){
         fmt::println("Extension: {} - The BMP file format or bitmap,\nis a raster graphics image file format used to store bitmap digital images, independently of the display device\n(such as a graphics adapter), especially on Microsoft Windows[2] and OS/2[3] operating systems.", file.extension().string());
-        auto image = sf::Image();
-        image.loadFromFile(file.string());
-        auto sizevec = image.getSize();
-        fmt::println("BMP Width: {}, BMP Height: {}", sizevec.x, sizevec.y);
-
-        //auto bmp_info = bmp_reader::read_bmp(file, true);
+        display_image_info(image);
     }
-    else if(extension == ".png"){
+    else if(extension == ".png") {
         fmt::println("decompressing PNG file");
-        auto image = sf::Image();
-        image.loadFromFile(file.string());
-        auto sizevec = image.getSize();
-        fmt::println("PNG Width: {}, PNG Height: {}", sizevec.x, sizevec.y);
-        for(int i = 0; i < sizevec.x*sizevec.y; i++){
-            auto color = image.getPixel(i%sizevec.x,i/sizevec.x);
-            //fmt::println("Pixel[{}] color values: r{} g{} b{}", i, color.r, color.g, color.b);
-        }
+        display_image_info(image);
     }
-
+    else if(extension == ".tga"){
+        fmt::println("decompressing TGA file");
+        display_image_info(image);
+    }
+    else if(extension == ".jpg"){
+        fmt::println("decompressing jpg file");
+        display_image_info(image);
+    }
+    else if(extension == ".gif"){
+        fmt::println("decompressing gif file");
+        display_image_info(image);
+    }
+    else if(extension == ".psd"){
+        fmt::println("decompressing psd file");
+        display_image_info(image);
+    }
+    else if(extension == ".hdr"){
+        fmt::println("decompressing hdr file");
+        display_image_info(image);
+    }
+    else if(extension == ".pic"){
+        fmt::println("decompressing TGA file");
+        display_image_info(image);
+    }
+    else if(extension == ".tga"){
+        fmt::println("decompressing TGA file");
+        display_image_info(image);
+    }
     else if(extension == ".ppm"){
 
     }
+    else if(extension == ".pgm"){
+
+    }
     else{
-        fmt::println("Extension: {} - Unsupported File Extension", file.extension().string());
+        fmt::println("Unsupported File Extension: {}", file.extension().string());
     }
 
 }
@@ -74,8 +109,7 @@ int main(int argc, char** argv) {
             return 0;
         }
         else {
-            display_file_info(path);
-            fmt::println("done!");
+            handle_file_extension(path);
         }
 
     }
